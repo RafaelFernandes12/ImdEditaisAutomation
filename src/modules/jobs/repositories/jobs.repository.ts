cdb45c947e82aab753bdbde652994900232c4632
RafaelFernandes12 <rfsilva786@gmail.com>
2026-09-23 14:54:11 -0300

    fix: conserta problema de busca pelo title always undefined

--- src/modules/jobs/repositories/jobs.repository.ts
+++ src/modules/jobs/repositories/jobs.repository.ts
@@ -23,7 +23,7 @@ export class JobsRepository {
         : { jerimum: { create: data.jerimum } };
 
     const job = await tx.job.upsert({
-      where: { title: data.title },
+      where: { title: data.link },
       create: {
         title: data.title,
         type: data.type,
